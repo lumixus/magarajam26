0892e69588c47cf0d40c5f4c005c22cc8d0d9152
Emir Taşkın <emirtaskin123@gmail.com>
2025-09-20 16:17:21 +0300

    [FEATURE] Add new switch button

--- Assets/Scripts/SwitchButton.cs
+++ Assets/Scripts/SwitchButton.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class SwitchButton : MonoBehaviour, IInteractable
 {
+    public bool isPressed = false;
+    public Sprite pressedSprite;
+    public Sprite defaultSprite;
+    public SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
     public void Interact()
     {
+        isPressed = true;
+        sr.sprite = pressedSprite;
         GameManager.instance.StartGame();
     }
 }
